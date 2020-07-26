8ec1b406b1142d547f21dc73d8728d7a48f70877
YoneKao <o1n1o1g1a@gmail.com>
2020-07-26 14:53:51 +0900

    Needleオブジェクト追加

--- Assets/Object/Example/Script/Needle.cs
+++ Assets/Object/Example/Script/Needle.cs
@@ -2,28 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paper : MonoBehaviour
+public class Needle : MonoBehaviour
 {
-
-    public enum PAPER_REVERSE //裏表プロパティ
-    {
-        OMOTE, //表
-        URA, //裏
-    }
-
-    public enum PAPER_ORGANIZE //整頓プロパティ
-    {
-        GOOD, //整理されている
-        BAD, //整理されていない
-    }
-
-    public PAPER_REVERSE reverse = PAPER_REVERSE.OMOTE;
-    public PAPER_ORGANIZE organize = PAPER_ORGANIZE.BAD;
-
     public AudioSource audioSource;
     public AudioClip clipStart;
     public AudioClip clipEnd;
-
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +14,7 @@ public class Paper : MonoBehaviour
         //生成時にSEを鳴らす
         audioSource = GetComponent<AudioSource>();
         audioSource.PlayOneShot(clipStart);
-        Debug.Log("Paperの生成時SEが再生されました");
+        Debug.Log("Needleの生成時SEが再生されました");
     }
 
     // Update is called once per frame
@@ -39,12 +22,11 @@ public class Paper : MonoBehaviour
     {
         
     }
-
     private void OnDestroy()
     {
         //破棄時にSEを鳴らす
         audioSource = GetComponent<AudioSource>();
         audioSource.PlayOneShot(clipEnd);
-        Debug.Log("Paperの破棄時SEが再生されました");
+        Debug.Log("Needleの破棄時SEが再生されました");
     }
 }
